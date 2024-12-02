9c56e7ba2b4d3b7cd68c3ca296f6e80188ed7156
Sravya200 <76867728+Sravya200@users.noreply.github.com>
2024-12-02 21:03:14 +0530

    Delete src directory

--- src/models/index.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('grocery_db', 'root', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-export default sequelize;
